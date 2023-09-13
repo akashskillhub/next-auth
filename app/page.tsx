3e6c706e8f62e387b6b51cd500ddf34f6b743ba9
akash <akash@gmail.com>
2023-09-13 08:34:47 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react"
 const Home = () => {
+  const { data, status } = useSession()
+
+  if (status === "authenticated") {
+    return <>
+      <button onClick={e => signOut()}>Logout</button>
+      <pre>{JSON.stringify(data)}</pre>
+
+      <h1>{data.user?.name}</h1>
+      <h1>{data.user?.email}</h1>
+      <img
+        src={data.user?.image as string}
+        alt={data.user?.name as string} />
+    </>
+  }
   return <>
-    Home Page
+    <button onClick={e => signIn("google")}>Login With Google</button>
   </>
 }
 
